d0a196576f2fa544ead27b7b255fd2b7f66d301c
Anokino <nikolas.loizeau@gmail.com>
2022-11-18 15:41:03 +0100

    ionic init

---
+++ hackat-event/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hackat-event',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
