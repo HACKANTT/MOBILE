cc560f2c0787177bd6debb4016376a8a16e5577e
Anokino <nikolas.loizeau@gmail.com>
2022-11-18 17:05:58 +0100

    Co-authored-by: MLM3-yt <MLM3-yt@users.noreply.github.com>

--- hackat-event/src/app/home/home.page.ts
+++ hackat-event/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,10 @@ import { NavigationExtras, Router } from '@angular/router';
 export class HomePage {
 
   lsthackathons:any;
-  constructor(private router: Router) {
-    fetch('assets/json/films.json').then(res => res.json())
-    .then(json => {
-      this.lsthackathons = json;
-    }
-    );
+  constructor(private http : HttpClient, private router: Router) {
+    this.http.get('http://localhost:8001/api/hackathons').subscribe(results => {
+      results=this.lsthackathons;
+    });
   }
 
   /*affDetail(hackathon){
