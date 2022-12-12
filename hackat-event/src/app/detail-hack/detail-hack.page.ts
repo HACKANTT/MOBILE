134a264b97f41229383b1f50255c23cf73739d85
Anokino <nikolas.loizeau@gmail.com>
2022-12-12 15:55:58 +0100

    map bugguée detail hack

--- hackat-event/src/app/detail-hack/detail-hack.page.ts
+++ hackat-event/src/app/detail-hack/detail-hack.page.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute} from '@angular/router';
-import { bindCallback } from 'rxjs';
+
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-detail-hack',
@@ -10,15 +11,22 @@ import { bindCallback } from 'rxjs';
 })
 export class DetailHackPage implements OnInit {
 
-
+  mapURL: SafeResourceUrl;
   hack: any;
-  constructor(private router: Router, private http: HttpClient, private activeRoute: ActivatedRoute) {
+  constructor(public sanitizer:DomSanitizer, private router: Router, private http: HttpClient, private activeRoute: ActivatedRoute) {
+    let url =
+      'https://www.google.com/maps/embed/v1/place?key=AIzaSyBBsM0iiWw3EHaTaHnRYtYPWq5GeBEM2pE&q={{hack.lieu}}&zoom=8';
+    this.mapURL = this.sanitizer.bypassSecurityTrustResourceUrl(url);
     this.activeRoute.queryParams.subscribe(params => {
     let item:any = this.router.getCurrentNavigation()?.extras.state;
     this.hack= item.hack;
     console.log(this.hack);
   }
   )}
+
+  sanitizeUrl(url:any){
+    return  this.sanitizer.bypassSecurityTrustUrl(url);
+  }
 
 
 
