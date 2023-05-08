206f9d1b53a934e9e42b83efb69a79b497f91173
Anokino <nkoloiz58@gmail.com>
2023-05-09 00:20:53 +0200

    recherche hackathons

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { FilterPipe } from '../filter.pipe';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, FilterPipe]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,6 +11,7 @@ import { HttpClient } from '@angular/common/http';
 export class HomePage {
 
   lsthackathons:any;
+  searchVille: string = '';
   constructor(private http : HttpClient, private router: Router) {
     this.http.get('http://'+ window.location.hostname+':8001/api/hackathons').subscribe(results => {
       this.lsthackathons=results;
